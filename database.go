14fb723bc0da609252dfd986e0c28e341ce3baf9
AFCMS <afcm.contact@gmail.com>
2022-10-18 23:29:14 +0200

    Fix database creation

--- database.go
+++ database.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"log"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"log"
 )
 
 type Entry struct {
@@ -13,7 +14,7 @@ type Entry struct {
 	Description  string `gorm:"not null;unique"`
 	CreationDate int64  `gorm:"not null"`
 	MeetingID    int32  `gorm:"not null;unique;check:meeting_id between 0 and 999999999"`
-	Passcode     string `gorm:"default:"`
+	Passcode     string `gorm:""`
 }
 
 func InitDatabase() *gorm.DB {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import "./App.css"
 import { useEffect, useState } from "react"
-import { PlusIcon, ArrowUpRightIcon, ComputerDesktopIcon } from "@heroicons/react/24/solid"
+import { PlusIcon, ArrowUpRightIcon, ComputerDesktopIcon, CommandLineIcon } from "@heroicons/react/24/solid"
 
 /**
  * @param {string} val
@@ -34,30 +34,32 @@ function App() {
 	const [addPasscode, setAddPasscode] = useState("")
 	const [addOpen, setAddOpen] = useState(false)
 
+	const [reload, setReload] = useState(0)
+
 	useEffect(() => {
 		axios
 			.get(url + "api/all")
 			.then((response) => {
-				//console.log(response);
-				//console.log(response.data);
 				setLoading(false)
 				setEntries(response.data)
 			})
 			.catch(() => {
 				setError("An error occured")
 			})
-	}, [])
+	}, [reload])
 
 	function create_entry() {
 		setLoadingCreate(true)
 		axios
-			.post(
-				url + "api/create",
-				JSON.stringify({ description: addDescription, meeting_id: addMeetingID, passcode: addPasscode })
-			)
+			.post(url + "api/create", {
+				description: addDescription,
+				meeting_id: parseInt(addMeetingID),
+				passcode: addPasscode,
+			})
 			.then((response) => {
 				console.log(response)
 				setLoadingCreate(false)
+				setReload(reload + 1)
 			})
 			.catch(() => {
 				setLoadingCreate(false)
@@ -68,11 +70,16 @@ function App() {
 		<div className="flex w-screen flex-row items-center justify-center bg-slate-100 p-0 md:p-2">
 			<div className="flex w-full columns-3 flex-col bg-slate-200 md:w-2/6 md:rounded md:border md:border-slate-700">
 				<div className="flex h-16 w-full items-center bg-blue-500 p-5">
-					<h1 className="h-max text-3xl font-bold text-blue-900">Zoom Link Server</h1>
-					<div className="w-max"></div>
-					<a className="w-max" href="https://github.com/AFCMS">
-						<span className="hidden text-blue-800 md:block">Made by AFCM</span>
-					</a>
+					<h1 className="h-max w-2/3 text-3xl font-bold text-blue-900">Zoom Link Server</h1>
+					<div className="w-1/3">
+						<a
+							className="text-right text-blue-800 md:block"
+							href="https://github.com/AFCMS/zoom-link-server"
+						>
+							<span className="hidden md:inline">Made by AFCM</span>
+							<CommandLineIcon className="right-0 h-8 w-8 md:hidden" />
+						</a>
+					</div>
 				</div>
 
 				<div className="flex h-12 w-full items-center justify-items-end bg-blue-300 p-2">
@@ -163,59 +170,63 @@ function App() {
 						} else {
 							return (
 								<>
-									{entries.map((e) => {
-										return (
-											<div
-												className={
-													"mt-2 flex h-20 w-full items-center rounded bg-zinc-300 p-2 pl-4"
-												}
-												key={e.id}
-											>
-												<div className="flex w-3/4 flex-col">
-													<div className="h-max text-xl text-black">{e.description}</div>
-													<div className="text-start mt-1 h-max text-lg text-slate-700">
-														<span className="mr-4">Meeting ID:</span>
-														<span className="mr-4">{e.meeting_id}</span>
+									{entries === null ? (
+										<div className="mt-2 items-center">No Entries</div>
+									) : (
+										entries.map((e) => {
+											return (
+												<div
+													className={
+														"mt-2 flex h-20 w-full items-center rounded bg-zinc-300 p-2 pl-4"
+													}
+													key={e.id}
+												>
+													<div className="flex w-3/4 flex-col">
+														<div className="h-max text-xl text-black">{e.description}</div>
+														<div className="text-start mt-1 h-max text-lg text-slate-700">
+															<span className="mr-4">Meeting ID:</span>
+															<span className="mr-4">{e.meeting_id}</span>
 
-														{(() => {
-															if (e.passcode && e.passcode.lenght !== 0) {
-																return (
-																	<>
-																		<span className="mr-4 hidden md:inline">
-																			Passcode:
-																		</span>
-																		<span className="hidden md:inline">
-																			{e.passcode}
-																		</span>
-																	</>
-																)
-															}
-														})()}
+															{(() => {
+																if (e.passcode && e.passcode.lenght !== 0) {
+																	return (
+																		<>
+																			<span className="mr-4 hidden md:inline">
+																				Passcode:
+																			</span>
+																			<span className="hidden md:inline">
+																				{e.passcode}
+																			</span>
+																		</>
+																	)
+																}
+															})()}
+														</div>
+													</div>
+													<div className="flex w-1/4 flex-col pt-2 pb-2">
+														<a
+															className="flex flex-row rounded bg-blue-500 p-1 text-sm"
+															href={`https://zoom.us/j/${e.meeting_id}`}
+															target="_blank"
+															rel="noopener noreferrer"
+														>
+															<span className="hidden md:inline">Open in Browser</span>
+															<span className="inline md:hidden">Browser</span>
+															<ArrowUpRightIcon className="ml-1 inline h-5" />
+														</a>
+														<a
+															className="mt-2 flex flex-row rounded bg-blue-500 p-1 text-sm"
+															href="zoomus://"
+														>
+															<span className="hidden md:inline">Open in App</span>
+															<span className="inline md:hidden">App</span>
+															<ComputerDesktopIcon className="ml-8 inline h-5" />
+														</a>
 													</div>
 												</div>
-												<div className="flex w-1/4 flex-col pt-2 pb-2">
-													<a
-														className="flex flex-row rounded bg-blue-500 p-1 text-sm"
-														href={`https://zoom.us/j/${e.meeting_id}`}
-														target="_blank"
-														rel="noopener noreferrer"
-													>
-														<span className="hidden md:inline">Open in Browser</span>
-														<span className="inline md:hidden">Browser</span>
-														<ArrowUpRightIcon className="ml-1 inline h-5" />
-													</a>
-													<a
-														className="mt-2 flex flex-row rounded bg-blue-500 p-1 text-sm"
-														href="zoomus://"
-													>
-														<span className="hidden md:inline">Open in App</span>
-														<span className="inline md:hidden">App</span>
-														<ComputerDesktopIcon className="ml-8 inline h-5" />
-													</a>
-												</div>
-											</div>
-										)
-									})}
+											)
+										})
+									)}
 								</>
 							)
 						}
