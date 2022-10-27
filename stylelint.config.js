db84b2bf04582fbe3b274c376c474609bbde9a2c
AFCMS <afcm.contact@gmail.com>
2022-10-27 17:36:59 +0200

    Add stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ["stylelint-config-standard"],
+    customSyntax: "postcss-scss",
+    rules: {
+        "at-rule-no-unknown": [
+            true,
+            {
+                ignoreAtRules: ["tailwind", "apply", "variants", "responsive", "screen", "layer"],
+            },
+        ],
+        indentation: null,
+        "value-list-comma-newline-after": null,
+        "declaration-colon-newline-after": null,
+    },
+    //ignoreFiles: ["/node_modules", "/build"],
+}
